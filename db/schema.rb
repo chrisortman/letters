cf24f49f647f102fac4c0b67afc84f3f528b9458
Chris Ortman <chris-ortman@uiowa.edu>
2015-01-24 23:06:44 -0600

    Format the new page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150125041858) do
+ActiveRecord::Schema.define(version: 20150125042256) do
+
+  create_table "letters", force: :cascade do |t|
+    t.string   "from"
+    t.string   "text"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
