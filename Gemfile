867a5ad53f303046fbb8e5a77258356c137041bb
Chris Ortman <chris-ortman@uiowa.edu>
2015-03-05 21:50:53 -0600

    WIP

--- Gemfile
+++ Gemfile
@@ -41,6 +41,8 @@ group :development, :test do
 
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
+
+  gem 'rspec-rails', '~> 3.0'
 end
 
 
@@ -54,3 +56,4 @@ gem 'bootstrap-sass', '~> 3.3.1'
 gem 'griddler'
 gem 'griddler-mandrill'
 gem 'medium-editor-rails', '~> 1.0.0'
+
--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -2,18 +2,6 @@ require 'test_helper'
 
 class HomeControllerTest < ActionController::TestCase
 
-  test 'when not logged in redirects to login url' do
-    get :index
-
-    assert_redirected_to login_url
-  end
-
-  test 'when session id is bogus redirects to login url' do
-    get :index, {}, {:user_id => 99}
-
-    assert_redirected_to login_url
-  end
-
   test 'assigns the current user if logged in' do
     get :index, {}, {:user_id => users(:chris).id}
 
