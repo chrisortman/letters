bad68006e552087f47cf0a885c9136f92a9cf90b
Chris Ortman <chris-ortman@uiowa.edu>
2015-03-18 14:56:29 -0500

    Use subscription instead of user for counts

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @subscriber_count = User.count
+    @subscriber_count = Subscription.count
     @story_count = Story.count
     @letter_count = Letter.count
   end
