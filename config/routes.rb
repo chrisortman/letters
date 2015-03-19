ed8cc64976f32cfd7e915549f600177f292c3f9c
Chris Ortman <chris-ortman@uiowa.edu>
2015-03-19 11:16:23 -0500

    Switching to posgres becuz heroku
    Had a hard time getting pg gem compiled so created
    new gemset

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
   get '/auth/failure', to: 'sessions#login_error'
   get 'sessions/create'
   get 'sessions/new'
+  get 'home', to: 'home#index',  as: :home
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
